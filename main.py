b0494632e160eafb70d480f974822f65e122a574
maddyboi7607 <mm9600432080@gmail.com>
2024-05-28 19:11:16 +0530

    Done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("DISTANCE CALCULATOR")
 while True:
     x1,y1=list(map(int,input("Enter point 1's co-ordinates").split(",")))
     x2,y2=list(map(int,input("Enter point 2's co-ordinates").split(",")))
-    Dist=(((x2-x1)*2)+((y2-y1)2))*0.5
+    Dist=(((x2-x1)**2)+((y2-y1)**2))**0.5
     print("Distance between 2 points=",round(Dist,3))
     print("                                             ======")
     a=input("Do you want to continue or exit? (y/n)")
